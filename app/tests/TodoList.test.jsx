9fd49cbd15b194288e653f5a321ab334e30c1e45
vladserban <serban.vlad@gmail.com>
2016-09-09 22:34:35 +0300

    Added special test for checking number of rendered components

--- app/tests/TodoList.test.jsx
+++ app/tests/TodoList.test.jsx
@@ -5,9 +5,26 @@ var $ = require('jquery');
 var expect = require('expect');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe("TodoList", () => {
   it("should exist", () => {
     expect(TodoList).toExist();
+  });
+
+  it("should render the correct number of Todos", () => {
+    var todos = [{
+      id: 1,
+      text: 'first'
+    },{
+      id: 2,
+      text: 'second one'
+    }];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+
   });
 });
