0314cbe98a200c452b29dd492322721fb54c357a
vladserban <serban.vlad@gmail.com>
2016-09-15 20:43:42 +0300

    added tests for toggling

--- app/tests/Todo.test.jsx
+++ app/tests/Todo.test.jsx
@@ -10,4 +10,23 @@ describe("Todo", () => {
   it("should exist", () => {
     expect(Todo).toExist();
   });
+
+
+  it("should fire onToggle with the correct id on click", () => {
+    var todoData = {
+      id: 119,
+      text: 'just another task',
+      completed: true
+    };
+
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy} />);
+
+    var $el = $(ReactDOM.findDOMNode(todo));
+
+    TestUtils.Simulate.click($el[0]);
+
+    expect(spy).toHaveBeenCalledWith(119);
+
+  });
 });
--- app/tests/TodoApp.test.jsx
+++ app/tests/TodoApp.test.jsx
@@ -11,7 +11,7 @@ describe("TodoApp", () => {
     expect(TodoApp).toExist();
   });
 
-  if("should add a new todo to the state", () => {
+  it("should add a new todo to the state", () => {
 
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
     todoApp.setState({ todos: [] });
@@ -22,4 +22,28 @@ describe("TodoApp", () => {
     expect(todoApp.state.todos[0].text).toBe(todoText);
 
   });
+
+  it("should toggle completed state", () => {
+
+    var todoData = {
+      id: 11,
+      text: 'Test task',
+      completed: false
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+    todoApp.setState({
+      todos: [todoData]
+    });
+
+    // check that todos first item completed flag is false
+    expect(todoApp.state.todos[0].completed).toBe(false);
+
+    // toggle it
+    todoApp.handleToggle(11);
+
+    // check toggle result
+    expect(todoApp.state.todos[0].completed).toBe(true);
+
+  });
+
 });
