ce01aef148eea93446c54c1773784273d025efeb
Vlad Serban <vlad@sb24.ocenture.int>
2016-09-15 15:25:06 +0300

    Wired add todo

--- app/tests/TodoApp.test.jsx
+++ app/tests/TodoApp.test.jsx
@@ -10,4 +10,16 @@ describe("TodoApp", () => {
   it("should exist", () => {
     expect(TodoApp).toExist();
   });
+
+  if("should add a new todo to the state", () => {
+
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+    todoApp.setState({ todos: [] });
+
+    var todoText = 'Za new todo';
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+
+  });
 });
