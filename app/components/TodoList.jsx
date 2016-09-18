6103ddcb1d0b2941bdce7556a1f19f5de4079dd3
vladserban <serban.vlad@gmail.com>
2016-09-18 22:32:03 +0300

    Fixed bug of not displaying any todos or `Nothing to do` message when all localStorage todos are completed and showCompleted is false

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -9,13 +9,15 @@ export var TodoList = React.createClass({
     var {todos, showCompleted, searchText} = this.props;
 
     var renderTodos = () => {
-      if (todos.length === 0 ){
+      var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+
+      if (filteredTodos.length === 0 ){
         return (
           <p className="container__message">Nothing to do. Yey!</p>
         );
       }
 
-      return TodoAPI.filterTodos(todos, showCompleted, searchText).map(
+      return filteredTodos.map(
         (todo) => <Todo key={todo.id} {...todo} />
       );
     }
