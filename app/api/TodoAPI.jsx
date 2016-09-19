cf93d800ca66bcfc202bfb2566cfdb50b98271ed
Vlad Serban <vlad@sb24.ocenture.int>
2016-09-19 18:24:18 +0300

    Fixed case sensitive bug in search filter

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -31,7 +31,7 @@ module.exports = {
     // filter by searchText
     filteredTodos = filteredTodos.filter( (todo) => {
       var todoText = todo.text.toLowerCase();
-      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+      return searchText.length === 0 || todoText.indexOf(searchText.toLowerCase()) > -1;
     });
 
     // sort todos with non-completed first
