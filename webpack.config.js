ca8a32a6ea1df3e91a92494ee6402fab9f204211
vladserban <serban.vlad@gmail.com>
2016-09-20 19:58:29 +0300

    Optimized webpack to run on production (heroku)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var webpack = require('webpack');
 var path = require('path');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -15,6 +18,11 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
     })
   ],
   output: {
@@ -58,5 +66,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined: 'eval-source-map'
 };
